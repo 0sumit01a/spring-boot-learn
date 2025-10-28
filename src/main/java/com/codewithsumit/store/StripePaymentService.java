d9bf2c92af03b2ab59a5c0a9c8ffcff553d78788
Sumit Singh Bisht <0sumit01a@gmail.com>
2025-10-28 23:39:45 +0530

    Externalizing configuration

--- src/main/java/com/codewithsumit/store/StripePaymentService.java
+++ src/main/java/com/codewithsumit/store/StripePaymentService.java
@@ -1,5 +1,6 @@
 package com.codewithsumit.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +8,25 @@ import org.springframework.stereotype.Service;
 @Service("stripe")
 @Primary
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${stripe.enabled}")
+    private String ena;
+
+    @Value("${stripe.timeout}")
+    private String timeOut;
+
+    @Value("${stripe.supported-currencies}")
+    private String sc;
+
     @Override
     public void processPayment(double amount){
+        System.out.println("url " + apiUrl);
+        System.out.println("isEnable " + ena);
+        System.out.println("time is out " + timeOut);
+        System.out.println("supported currencies " + sc);
+
         System.out.println("stripped");
         System.out.println("Amount : "+ amount );
     }
